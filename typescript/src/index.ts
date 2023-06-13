17e1e5adf84072654dfbfa15df0236f915ad2f42
Andreas Blech <andreas.blech@t-online.de>
2023-06-13 19:00:01 +0200

    index.d.ts can be compiled. index.ts is not complete yet.

---
+++ typescript/src/index.ts
@@ -0,0 +1,9 @@
+
+import { Template } from 'model/template';
+
+import { SpecificProperty } from 'model/specific_property';
+
+import { SpecificProperties } from 'model/specific_properties';
+
+export { Template, SpecificProperty, SpecificProperties };
+
