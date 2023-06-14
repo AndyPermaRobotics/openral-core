4a09365b8b971086d085c19e7082337debb0857e
Andreas Blech <andreas.blech@t-online.de>
2023-06-14 13:36:53 +0200

    updated typescript example

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,7 +1,41 @@
 
-import { Template } from 'openral-js';
+import { Container, CurrentGeoLocation, Definition, Identity, RalMethod, RalObject, RalObjectWithRole, SpecificProperties, SpecificProperty, Template } from 'openral-js';
+
+//this is how to create a RalObject
+let myRalObject: RalObject = new RalObject({
+    identity: new Identity("myUid"),
+    definition: new Definition("An Example RalObject"),
+    template: new Template("exampleObject", "1.0", null),
+    specificProperties: new SpecificProperties({
+        "myPropertyKey": new SpecificProperty("myPropertyKey", "value"),
+    }),
+    currentGeoLocation: new CurrentGeoLocation(new Container("containerUid")),
+});
+
+console.log(myRalObject);
 
 
-const template = new Template("ralType", "1.0")
+//we can use the ralObject as an Input Object for a RalMethod by adding a role and creating a RalObjectWithRole
 
-console.log(template.ralType);
+const inputObject = RalObjectWithRole.fromRalObjectPlusRole(
+    myRalObject, "roleOfRalObject")
+
+
+const myRalmethod = new RalMethod(
+    {
+        identity: new Identity("methodUid"),
+        definition: new Definition(),
+        specificProperties: new SpecificProperties({}),
+        template: new Template("methodeRalType", "1.0"),
+        methodState: "undefined",
+        inputObjects: [
+            inputObject,
+        ],
+        outputObjects: [],
+        nestedMethods: [],
+        inputObjectsRef: [],
+        outputObjectsRef: [],
+    }
+);
+
+console.log(myRalmethod);
