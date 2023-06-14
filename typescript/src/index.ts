fb1efcdad56a73db2b3acb30712752b924ff0638
Andreas Blech <andreas.blech@t-online.de>
2023-06-14 13:37:08 +0200

    index.ts exports all relevant types

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,12 +1,20 @@
 
-import { Template } from 'model/template';
-
-import { SpecificProperty } from 'model/specific_property';
-
-import { SpecificProperties } from 'model/specific_properties';
-
+//models
+export { Container } from 'model/container';
+export { CurrentGeoLocation } from 'model/current_geo_location';
+export { Definition } from 'model/definition';
+export { Identity } from 'model/identity';
+export { ObjectRef } from 'model/object_ref';
+export { RalMethod } from 'model/ral_method';
+export { RalObject } from 'model/ral_object';
+export { RalObjectWithRole } from 'model/ral_object_with_role';
+export { SpecificProperties } from 'model/specific_properties';
+export { SpecificProperty } from 'model/specific_property';
+export { Template } from 'model/template';
+// repositories
 export { RalMethodRepository } from 'repository/ral_method_repository';
 export { RalObjectRepository } from 'repository/ral_object_repository';
-export { Template, SpecificProperty, SpecificProperties };
+
+
 
 
