854cc87617bd8205e35fad3c27ab58ce2d2603e8
Andreas Blech <andreas.blech@t-online.de>
2023-06-22 16:28:19 +0200

    library exports for dart openral_core package

--- dart/lib/openral_core.dart
+++ dart/lib/openral_core.dart
@@ -1,8 +1,20 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Core library for OpenRAL
 library openral_core;
 
-export 'src/openral_core_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/discovery/discovery_dimension.dart';
+export 'src/discovery/discovery_v2.dart';
+export 'src/discovery/graph_node.dart';
+export 'src/model/container.dart';
+export 'src/model/current_geo_location.dart';
+export 'src/model/definition.dart';
+export 'src/model/identity.dart';
+export 'src/model/object_ref.dart';
+export 'src/model/ral_method.dart';
+export 'src/model/ral_object.dart';
+export 'src/model/ral_object_with_role.dart';
+export 'src/model/specific_properties.dart';
+export 'src/model/specific_property.dart';
+export 'src/model/template.dart';
+export 'src/repository/mock_ral_object_repository.dart';
+export 'src/repository/ral_method_repository.dart';
+export 'src/repository/ral_object_repository.dart';
