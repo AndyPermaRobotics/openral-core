b4c44f4bdd20c298e394184d7757898c433829b6
Andreas Blech <blech@permarobotics.com>
2023-08-21 16:55:27 +0200

    added updateMethod and updateByUid methods to ralmethodrepository

--- dart/lib/src/repository/ral_method_repository.dart
+++ dart/lib/src/repository/ral_method_repository.dart
@@ -11,4 +11,34 @@ abstract class RalMethodRepository {
 
   /// Creates a new [RalObject] in the database. If [overrideIfExists] is true, the [RalObject] will be overwritten if it already exists. Otherwise, an error will be thrown.
   Future<void> create(RalMethod ralMethod, {required bool overrideIfExists});
+
+  /// updates the given [RalMethod]
+  /// the modification must be done in the [modificationFunc] function, which provides an update to date version of the [RalMethod] as a parameter, to avoid overwriting changes
+  Future<void> updateMethod({
+    required RalMethod ralObject,
+    required RalMethod Function(RalMethod method) modificationFunc,
+  }) async {
+    await updateByUid(
+      uid: ralObject.identity.uid,
+      modificationFunc: modificationFunc,
+    );
+  }
+
+  /// updates the given RalObject with the given uid
+  /// the modification must be done in the [modificationFunc] function, which provides an update to date version of the [RalMethod] as a parameter, to avoid overwriting changes
+  Future<void> updateByUid({
+    required String uid,
+    required RalMethod Function(RalMethod object) modificationFunc,
+  }) async {
+    //Note: if the specific implementation of [RalMethodRepository] supports transactions, this should be done in a transaction
+
+    final object = await getByUid(uid);
+
+    final modifiedObject = modificationFunc(object);
+
+    await create(
+      modifiedObject,
+      overrideIfExists: true,
+    );
+  }
 }
